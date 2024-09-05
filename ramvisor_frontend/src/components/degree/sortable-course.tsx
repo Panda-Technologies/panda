e95b76d06537ed0828d0817623ec8dee3cac9270
maximdolphin <maximka.dolphin@gmail.com>
2024-09-05 09:41:05 -0400

    add handleremove, getscheduleinfo for sort-course

--- ramvisor_frontend/src/components/degree/sortable-course.tsx
+++ ramvisor_frontend/src/components/degree/sortable-course.tsx
@@ -1,39 +1,59 @@
 import {
-    SortableContext,
-    arrayMove,
-    verticalListSortingStrategy,
-    useSortable,
-  } from "@dnd-kit/sortable";
+  SortableContext,
+  arrayMove,
+  verticalListSortingStrategy,
+  useSortable,
+} from "@dnd-kit/sortable";
 import React from "react";
 import { CSS } from "@dnd-kit/utilities";
 import { getClassColor } from "@utilities/helpers";
-import { Class, Degree } from "@graphql/generated/graphql";
+import { Class, Degree, DegreeSchedule } from "@graphql/generated/graphql";
 
 type Props = {
-    onRemoveFromSemester: (semesterId: string, courseId: string) => void;
-    course: Class;
-    semesterId?: string;
-    semesters: string[];
-    degree: Degree;
-}
+  onRemoveFromSemester: (semesterId: number, courseId: number) => void;
+  course: Class;
+  semesterId?: number;
+  DegreeSchedules?: DegreeSchedule[];
+  degree: Degree;
+};
 
-export const SortableCourse = ({ course, semesterId, onRemoveFromSemester, semesters, degree }: Props) => {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
-        id: course.id,
+export const SortableCourse = ({
+  course,
+  semesterId,
+  onRemoveFromSemester,
+  DegreeSchedules,
+  degree,
+}: Props) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({
+      id: course.id,
     });
 
-    const style: React.CSSProperties = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-        ...getClassColor(course, degree),
-        marginBottom: '4px',
-        borderRadius: '4px',
-        boxShadow: '0 1px 2px rgba(0, 0, 0, 0.05)',
-        display: 'flex',
-        alignItems: 'center',
-    };
+  const style: React.CSSProperties = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    ...getClassColor(course, degree),
+    marginBottom: "4px",
+    borderRadius: "4px",
+    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.05)",
+    display: "flex",
+    alignItems: "center",
+  };
 
-    const handleRemove = (event: React.MouseEvent) => {
-        event.preventDefault();
+  const handleRemove = (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (semesterId) {
+      onRemoveFromSemester(semesterId, course.id);
     }
+  };
+
+  const getScheduleInfo = () => {
+    if (semesterId && DegreeSchedules) {
+      for (const schedule of DegreeSchedules) {
+        if (schedule.entries?.find((entry) => entry?.classId === course.id)) {
+        }
+      }
+    }
+  };
 };
