c0170193f8553c7eaeacc29423f7ddfa09ee39ad
maximdolphin <maximka.dolphin@gmail.com>
2024-10-14 12:46:08 -0400

    Add null check to getclasscolor

--- ramvisor_frontend/src/utilities/helpers.ts
+++ ramvisor_frontend/src/utilities/helpers.ts
@@ -1,93 +1,96 @@
 import dayjs from "dayjs";
-import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import {GetFieldsFromList} from "@refinedev/nestjs-query";
 
-import { DashboardDealsChartQuery } from "@/graphql/types";
+import {DashboardDealsChartQuery} from "@/graphql/types";
 
 import nookies from "nookies";
-import { Class, Degree } from "@graphql/generated/graphql";
+import {Class, Degree} from "@graphql/generated/graphql";
 
 type DealStage = GetFieldsFromList<DashboardDealsChartQuery>;
 
 type DealAggregate = DealStage["dealsAggregate"][0];
 
 interface MappedDealData {
-  timeUnix: number;
-  timeText: string;
-  value: number;
-  state: string;
+    timeUnix: number;
+    timeText: string;
+    value: number;
+    state: string;
 }
 
 // Get the date in the format "MMM DD, YYYY - HH:mm"
 export const getDate = (startDate: string, endDate: string) => {
-  const start = dayjs(startDate).format("MMM DD, YYYY - HH:mm");
-  const end = dayjs(endDate).format("MMM DD, YYYY - HH:mm");
+    const start = dayjs(startDate).format("MMM DD, YYYY - HH:mm");
+    const end = dayjs(endDate).format("MMM DD, YYYY - HH:mm");
 
-  return `${start} - ${end}`;
+    return `${start} - ${end}`;
 };
 
 // Filter out deals that don't have a closeDateMonth or closeDateYear
 const filterDeal = (deal?: DealAggregate) =>
-  deal?.groupBy && deal.groupBy?.closeDateMonth && deal.groupBy?.closeDateYear;
+    deal?.groupBy && deal.groupBy?.closeDateMonth && deal.groupBy?.closeDateYear;
 
 const mapDeals = (
-  deals: DealAggregate[] = [],
-  state: string
+    deals: DealAggregate[] = [],
+    state: string
 ): MappedDealData[] => {
-  // filter out deals that don't have a closeDateMonth or closeDateYear
-  return deals.filter(filterDeal).map((deal) => {
-    // Get the closeDateMonth and closeDateYear from the deal
-    const { closeDateMonth, closeDateYear } = deal.groupBy as NonNullable<
-      DealAggregate["groupBy"]
-    >;
+    // filter out deals that don't have a closeDateMonth or closeDateYear
+    return deals.filter(filterDeal).map((deal) => {
+        // Get the closeDateMonth and closeDateYear from the deal
+        const {closeDateMonth, closeDateYear} = deal.groupBy as NonNullable<
+            DealAggregate["groupBy"]
+        >;
 
-    // Create a date object from the closeDateMonth and closeDateYear
-    const date = dayjs(`${closeDateYear}-${closeDateMonth}-01`);
+        // Create a date object from the closeDateMonth and closeDateYear
+        const date = dayjs(`${closeDateYear}-${closeDateMonth}-01`);
 
-    // Return the mapped deal data
-    return {
-      // Convert the date to a unix timestamp i.e., 1622505600000
-      timeUnix: date.unix(),
-      // Convert the date to a string i.e., "May 2021"
-      timeText: date.format("MMM YYYY"),
-      // Get the sum of all deals in this stage
-      value: deal.sum?.value ?? 0,
-      state,
-    };
-  });
+        // Return the mapped deal data
+        return {
+            // Convert the date to a unix timestamp i.e., 1622505600000
+            timeUnix: date.unix(),
+            // Convert the date to a string i.e., "May 2021"
+            timeText: date.format("MMM YYYY"),
+            // Get the sum of all deals in this stage
+            value: deal.sum?.value ?? 0,
+            state,
+        };
+    });
 };
 
 // Map deals data to the format required by the chart
 export const mapDealsData = (
-  dealStages: DealStage[] = []
+    dealStages: DealStage[] = []
 ): MappedDealData[] => {
-  // Get the deal stage with the title "WON"
-  const won = dealStages.find((stage) => stage.title === "WON");
-  const wonDeals = mapDeals(won?.dealsAggregate, "Won");
+    // Get the deal stage with the title "WON"
+    const won = dealStages.find((stage) => stage.title === "WON");
+    const wonDeals = mapDeals(won?.dealsAggregate, "Won");
 
-  // Get the deal stage with the title "LOST"
-  const lost = dealStages.find((stage) => stage.title === "LOST");
-  const lostDeals = mapDeals(lost?.dealsAggregate, "Lost");
+    // Get the deal stage with the title "LOST"
+    const lost = dealStages.find((stage) => stage.title === "LOST");
+    const lostDeals = mapDeals(lost?.dealsAggregate, "Lost");
 
-  // Combine the won and lost deals and sort them by time
-  return [...wonDeals, ...lostDeals].sort((a, b) => a.timeUnix - b.timeUnix);
+    // Combine the won and lost deals and sort them by time
+    return [...wonDeals, ...lostDeals].sort((a, b) => a.timeUnix - b.timeUnix);
 };
 
 export const getUserId = (): string => {
-  const cookies = nookies.get(null);
-  return cookies['user-id'];
+    const cookies = nookies.get(null);
+    return cookies['user-id'];
 }
 
-export const getClassColor = (course: Class, degree: Degree): React.CSSProperties => {
-  const colors: { [key: string]: React.CSSProperties } = {
-    core: { backgroundColor: "#e9d5ff", color: "#581c87" },
-    prerequisite: { backgroundColor: "#fce7f3", color: "#831843" },
-    elective: { backgroundColor: "#dbeafe", color: "#1e3a8a" },
-    default: { backgroundColor: "#e0e7ff", color: "#312e81" },
-  };
+export const getClassColor = (course: Class, degree: Degree | null): React.CSSProperties | null => {
+    if (degree) {
+        const colors: { [key: string]: React.CSSProperties } = {
+            core: {backgroundColor: "#e9d5ff", color: "#581c87"},
+            prerequisite: {backgroundColor: "#fce7f3", color: "#831843"},
+            elective: {backgroundColor: "#dbeafe", color: "#1e3a8a"},
+            default: {backgroundColor: "#e0e7ff", color: "#312e81"},
+        };
 
-  if (!course.electiveDegreeId || course.electiveDegreeId.length === 0) return colors.default;
-  if (course.category.includes('core')) return colors.core;
-  if (course.category.includes('elective')) return colors.elective;
+        if (!course.electiveDegreeId || course.electiveDegreeId.length === 0) return colors.default;
+        if (course.category.includes('core')) return colors.core;
+        if (course.category.includes('elective')) return colors.elective;
 
-  return colors.default;
+        return colors.default;
+    }
+    return null;
 };
