11416b60e1dc72eeeee96a8cc11e3a472ee23e8b
maximdolphin <maximka.dolphin@gmail.com>
2024-10-25 16:46:16 -0400

    Create component for fetching courses, taken out of degree page

---
+++ ramvisor_frontend/src/utilities/fetchClasses.ts
@@ -0,0 +1,37 @@
+// Fetch courses
+import {useList} from "@refinedev/core";
+import {GET_CLASSES_QUERY} from "@graphql/queries";
+import {Class} from "@graphql/generated/graphql";
+import {useCallback, useMemo} from "react";
+
+const CourseFetch = () => {
+    const {
+        data: coursesData,
+        isLoading: coursesLoading,
+        error: coursesError,
+    } = useList<Class>({
+        resource: "classes",
+        meta: {
+            gqlQuery: GET_CLASSES_QUERY,
+        },
+    });
+
+// Memoize courses map
+    const coursesMap = useMemo(() => {
+        const map = new Map<number, Class>();
+        coursesData?.data?.forEach((course) => {
+            if (course) map.set(course.id, course);
+        });
+        return map;
+    }, [coursesData]);
+
+// Get course function
+    const getCourse = useCallback(
+        (classId: number) => coursesMap.get(classId),
+        [coursesMap]
+    );
+
+    return { getCourse, coursesData, coursesLoading, coursesError };
+};
+
+export default CourseFetch;
