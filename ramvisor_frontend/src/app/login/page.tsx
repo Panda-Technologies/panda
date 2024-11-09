1420cf6ce895aa5fc583299d3175da9a7a7aa719
Ipapkin <Ipapkin04@gmail.com>
2024-11-09 16:31:51 -0500

    adding questionnare code(unfinished)

--- ramvisor_frontend/src/app/login/page.tsx
+++ ramvisor_frontend/src/app/login/page.tsx
@@ -2,23 +2,33 @@ import { AuthPage } from "@components/auth-page";
 import { authProviderServer } from "@providers/auth-provider";
 import { redirect } from "next/navigation";
 import React from "react";
+import { checkIfUserNeedsQuestionnaire } from "@utils/check-questionnaire";
+import {useGetIdentity} from "@refinedev/core"; // Import the function
 
 export default async function Login() {
-  const data = await getData();
+  const { data: identity } = useGetIdentity<{ id: string }>();
+  const userId = identity?.id;
 
-  if (data.authenticated) {
-    redirect(data?.redirectTo || "/");
+  if (userId) {
+    const userNeedsQuestionnaire = await checkIfUserNeedsQuestionnaire(userId);
+
+    if (userNeedsQuestionnaire) {
+      redirect("/multistepform"); // Redirect to the multi-step questionnaire
+    } else {
+      redirect("/dashboard"); // Redirect to the dashboard
+    }
   }
 
   return <AuthPage type="login" />;
 }
 
 async function getData() {
-  const { authenticated, redirectTo, error } = await authProviderServer.check();
+  const { authenticated, redirectTo, error, userId } = await authProviderServer.check();
 
   return {
     authenticated,
     redirectTo,
     error,
+    userId,
   };
 }
