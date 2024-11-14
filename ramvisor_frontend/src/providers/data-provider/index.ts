c60c6a3ee4a4b67487d4e77a7d63130c84818e7b
maximdolphin <maximka.dolphin@gmail.com>
2024-11-14 12:49:38 -0500

    Add credential handling for GQL

--- ramvisor_frontend/src/providers/data-provider/index.ts
+++ ramvisor_frontend/src/providers/data-provider/index.ts
@@ -10,8 +10,15 @@ import { createClient } from "graphql-ws";
 export const API_URL = "http://localhost:5001/graphql";
 const WS_URL = "ws://localhost:5001/graphql";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+const gqlClient = new GraphQLClient(API_URL, {
+    credentials: "include",
+    headers: {
+      'Content-Type': 'application/json',
+    }
+});
+const wsClient = createClient({ url: WS_URL, connectionParams: {
+  credentials: 'include'
+} });
 
 export const dataProvider = dataProviderNestjsQuery(gqlClient);
 export const liveProvider = liveProviderNestjsQuery(wsClient);
