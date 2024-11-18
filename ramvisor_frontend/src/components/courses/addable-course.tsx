4c4ec468d419471737d92f393ea4b536354f32e0
maximdolphin <maximka.dolphin@gmail.com>
2024-11-17 23:34:46 -0500

    Changing main scheduler component attributes

---
+++ ramvisor_frontend/src/components/courses/addable-course.tsx
@@ -0,0 +1,15 @@
+import {Course, AddableCourseCard, Section} from "@components/courses/calendar";
+
+const AddableCourse: React.FC<{ course: Course; section: Section }> = ({ course, section }) => {
+
+
+    return (
+        <AddableCourseCard color={course.color}>
+            <div style={{ fontWeight: 'bold' }}>{course.id}<br />{course.name}</div>
+            <div>{section.professor}</div>
+            <div>{section.days} {section.startTime} - {section.endTime}</div>
+        </AddableCourseCard>
+    );
+};
+
+export default AddableCourse;
