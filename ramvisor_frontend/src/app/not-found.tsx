5c68e1b713acc5b1994110720324e7f684d39d6c
maximdolphin <maximka.dolphin@gmail.com>
2024-11-23 23:53:44 -0500

    Added lazy loading to not found page to free ~3 mb on load

--- ramvisor_frontend/src/app/not-found.tsx
+++ ramvisor_frontend/src/app/not-found.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { ErrorComponent } from "@refinedev/antd";
+import dynamic from 'next/dynamic';
 import { Authenticated } from "@refinedev/core";
-import React from "react";
 import { Suspense } from "react";
 
+// Dynamic import for ErrorComponent
+const ErrorComponent = dynamic(
+    () => import("@refinedev/antd").then((mod) => mod.ErrorComponent),
+    {
+        ssr: false,
+        loading: () => <div>Loading error page...</div>
+    }
+);
+
 export default function NotFound() {
-  return (
-    <Suspense>
-      <Authenticated key="not-found">
-        <ErrorComponent />
-      </Authenticated>
-    </Suspense>
-  );
+    return (
+        <Suspense>
+            <Authenticated key="not-found">
+                <ErrorComponent />
+            </Authenticated>
+        </Suspense>
+    );
 }
