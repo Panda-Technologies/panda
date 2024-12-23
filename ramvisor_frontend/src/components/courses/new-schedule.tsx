f0e3aecceb5bb08243e27f724cca64b3292bb8c2
maximdolphin <maximka.dolphin@gmail.com>
2024-12-22 20:03:09 -0500

    Add new schedule modal

--- ramvisor_frontend/src/components/courses/new-schedule.tsx
+++ ramvisor_frontend/src/components/courses/new-schedule.tsx
@@ -1,44 +1,75 @@
 import { CREATE_CLASS_SCHEDULE_MUTATION } from "@graphql/mutations";
-import { useModalForm } from "@refinedev/antd";
-import { Form, Input, Modal } from "antd";
-import React from "react";
+import {Form, Input, message, Modal, Select} from "antd";
+import React, { useState } from "react";
+import { ClassSchedule, ClassScheduleEntry } from "@graphql/generated/graphql";
+import { useMutation } from "@apollo/client";
 
-type NewPlannerModalProps = {
+type NewScheduleModalProps = {
     onClose: () => void;
+    onFinish: (newSchedule: ClassSchedule) => void;
     semesterId: string;
+    semesters?: Array<{
+        id: string;
+    }>;
+    isVisible: boolean;
 };
 
-const NewPlannerModal: React.FC<NewPlannerModalProps> = ({
-                                                             onClose,
-                                                             semesterId,
-                                                         }) => {
-    const { formProps, modalProps, close } = useModalForm({
-        action: "create",
-        defaultVisible: true,
-        meta: {
-            gqlMutation: CREATE_CLASS_SCHEDULE_MUTATION,
+const NewScheduleModal: React.FC<NewScheduleModalProps> = ({
+                                                               onClose,
+                                                               semesterId,
+                                                               semesters = [],
+                                                               onFinish,
+                                                               isVisible,
+                                                           }) => {
+    const [form] = Form.useForm();
+
+    const [createSchedule, { loading }] = useMutation(CREATE_CLASS_SCHEDULE_MUTATION, {
+        onCompleted: (data) => {
+            const createdSchedule = data?.createClassSchedule;
+            if (createdSchedule) {
+                onFinish(createdSchedule as ClassSchedule);
+                message.success('Schedule successfully created', 0.5);
+                onClose();
+            } else {
+                console.error('Created schedule data is missing from the response');
+            }
         },
+        onError: (error) => {
+            console.log('Failed to create schedule', error);
+            message.error('Failed to create schedule');
+            throw new Error("Failed to create schedule");
+        }
     });
+
+    const handleSubmit = async (values: any) => {
+        await createSchedule({
+            variables: {
+                input: {
+                    title: values.title,
+                    semesterId: values.semesterId,
+                }
+            }
+        });
+    };
+
+    const handleCancel = () => {
+        onClose();
+    };
 
     return (
         <Modal
-            {...modalProps}
-            onCancel={() => {
-                close();
-                onClose();
-            }}
+            open={isVisible}
+            onCancel={handleCancel}
             title="Create new schedule"
             width={256}
+            okText="Create"
+            confirmLoading={loading}
+            onOk={() => form.submit()}
         >
             <Form
-                {...formProps}
+                form={form}
                 layout="vertical"
-                onFinish={(values) => {
-                    formProps?.onFinish?.({
-                        ...values,
-                        semesterId: semesterId,
-                    });
-                }}
+                onFinish={handleSubmit}
             >
                 <Form.Item
                     label="Title"
@@ -47,9 +78,23 @@ const NewPlannerModal: React.FC<NewPlannerModalProps> = ({
                 >
                     <Input />
                 </Form.Item>
+                <Form.Item
+                    label="Semester"
+                    name="semesterId"
+                    initialValue={semesterId}
+                    rules={[{ required: true, message: "Please select a semester" }]}
+                >
+                    <Select>
+                        {semesters.map((semester) => (
+                            <Select.Option key={semester.id} value={semester.id}>
+                                {semester.id}
+                            </Select.Option>
+                        ))}
+                    </Select>
+                </Form.Item>
             </Form>
         </Modal>
     );
 };
 
-export default NewPlannerModal;
+export default NewScheduleModal;
