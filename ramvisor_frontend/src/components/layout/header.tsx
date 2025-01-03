4984889b3c36edbbdf0fd3e73da431dbaa01aefe
maximdolphin <maximka.dolphin@gmail.com>
2025-01-03 10:40:50 -0500

    made header position relative instead of sticky to prevent awkward overflow handling

--- ramvisor_frontend/src/components/layout/header.tsx
+++ ramvisor_frontend/src/components/layout/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     justifyContent: 'flex-end',
     alignItems: 'center',
     padding: '0 24px',
-    position: 'sticky',
+    position: 'relative',
     top: 0,
     zIndex: 999,
     width: '100%',
