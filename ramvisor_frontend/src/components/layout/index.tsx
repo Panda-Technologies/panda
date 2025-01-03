b7812a6a40babf6eb10650e04468e956d7b9bea7
maximdolphin <maximka.dolphin@gmail.com>
2025-01-03 10:39:46 -0500

    adjust slight styling of layout of app

--- ramvisor_frontend/src/components/layout/index.tsx
+++ ramvisor_frontend/src/components/layout/index.tsx
@@ -7,18 +7,25 @@ import pandaIcon from "@config/panda.png"
 import Image from "next/image";
 import React from "react";
 
-
 const Layout = ({ children }: React.PropsWithChildren) => {
     return (
-      <ThemedLayoutV2
-        Sider={() => (
-          <ThemedSiderV2 Title={(titleProps) => <ThemedTitleV2 icon={<Image src={pandaIcon} alt="Panda" width={24} height={24} />} {...titleProps} text="Panda" />} />
-        )}
-        Header={Header}
-      >
-        {children}
-      </ThemedLayoutV2>
+        <ThemedLayoutV2
+            Sider={() => (
+                <ThemedSiderV2
+                    Title={(titleProps) =>
+                        <ThemedTitleV2
+                            icon={<Image src={pandaIcon} alt="Panda" width={24} height={24} />}
+                            {...titleProps}
+                            text="Panda"
+                        />
+                    }
+                />
+            )}
+            Header={Header}
+        >
+            {children}
+        </ThemedLayoutV2>
     );
-  };
+};
 
 export default Layout;
