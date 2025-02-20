4e6c4f4afca64152ed4431140b6e2fec0eb24636
maximdolphin <maximka.dolphin@gmail.com>
2025-02-19 20:59:34 -0500

    Rewrite new planner handling to use apollo and API return

--- ramvisor_frontend/src/components/degree/new-planner.tsx
+++ ramvisor_frontend/src/components/degree/new-planner.tsx
@@ -1,57 +1,94 @@
 import { CREATE_DEGREE_PLANNER_MUTATION } from "@graphql/mutations";
-import { useModalForm } from "@refinedev/antd";
-import { Form, Input, Modal } from "antd";
+import { Form, Input, message, Modal, Select } from "antd";
 import React from "react";
+import { Degree, DegreePlanner } from "@graphql/generated/graphql";
+import { useMutation } from "@apollo/client";
 
 type NewPlannerModalProps = {
   onClose: () => void;
-  userId: String;
-  degreeId: number;
+  onFinish: (newPlanner: DegreePlanner) => void;
+  degrees: Degree[];
+  isVisible: boolean;
 };
 
 const NewPlannerModal: React.FC<NewPlannerModalProps> = ({
-  onClose,
-  userId,
-  degreeId,
-}) => {
-  const { formProps, modalProps, close } = useModalForm({
-    action: "create",
-    defaultVisible: true,
-    meta: {
-      gqlMutation: CREATE_DEGREE_PLANNER_MUTATION,
+                                                           onClose,
+                                                           degrees,
+                                                           onFinish,
+                                                           isVisible,
+                                                         }) => {
+  const [form] = Form.useForm();
+
+  const [createPlanner, { loading }] = useMutation(CREATE_DEGREE_PLANNER_MUTATION, {
+    onCompleted: (data) => {
+      const createdPlanner = data?.createDegreePlanner;
+      if (createdPlanner) {
+        onFinish(createdPlanner as DegreePlanner);
+        message.success('Planner successfully created', 0.5);
+        onClose();
+      } else {
+        console.error('Created planner data is missing from the response');
+      }
     },
+    onError: (error) => {
+      console.log('Failed to create planner', error);
+      message.error('Failed to create planner');
+      throw new Error("Failed to create planner");
+    }
   });
 
+  const handleSubmit = async (values: any) => {
+    await createPlanner({
+      variables: {
+        input: {
+          title: values.title,
+          degreeId: values.degreeId,
+        }
+      }
+    });
+  };
+
+  const handleCancel = () => {
+    onClose();
+  };
+
   return (
-    <Modal
-      {...modalProps}
-      onCancel={() => {
-        close();
-        onClose();
-      }}
-      title="Create new planner"
-      width={256}
-    >
-      <Form
-        {...formProps}
-        layout="vertical"
-        onFinish={(values) => {
-          formProps?.onFinish?.({
-            ...values,
-            userId: userId,
-            degreeId: degreeId,
-          });
-        }}
+      <Modal
+          open={isVisible}
+          onCancel={handleCancel}
+          title="Create new planner"
+          width={256}
+          okText="Create"
+          confirmLoading={loading}
+          onOk={() => form.submit()}
       >
-        <Form.Item
-          label="Title"
-          name="title"
-          rules={[{ required: true, message: "Please enter a title" }]}
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={handleSubmit}
         >
-          <Input />
-        </Form.Item>
-      </Form>
-    </Modal>
+          <Form.Item
+              label="Select Degree"
+              name="degreeId"
+              rules={[{ required: true, message: "Please select a degree" }]}
+          >
+            <Select>
+              {degrees.map((degree) => (
+                  <Select.Option key={degree.id} value={degree.id}>
+                    {degree.name}
+                  </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+              label="Title"
+              name="title"
+              rules={[{ required: true, message: "Please enter a title" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
   );
 };
 
